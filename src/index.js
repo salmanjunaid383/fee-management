4f405cd6aa484a31c787387026275bceb51ab59d
staz6 <muhammad.aahad81@gmail.com>
2021-10-04 18:13:03 +0500

    view attendance inprogess

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import axios from "axios";
 
-axios
-.get(`http://fee-management-api.nastechltd.co/api/school_profile/${localStorage.getItem("school_id")}`).then((response) => {
-    console.log(response.data)
-    window.img=response.data;
-})
+
 ReactDOM.render(
     <>
         <BrowserRouter>
